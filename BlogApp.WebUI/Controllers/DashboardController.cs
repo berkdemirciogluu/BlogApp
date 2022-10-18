61c84bbbca568580b07e5a3a2c2d2ebad2f1ebc3
berkdemircioglu <berkdemircioglu@yahoo.com>
2022-10-19 01:18:18 +0300

    70.ders bitti

--- BlogApp.WebUI/Controllers/DashboardController.cs
+++ BlogApp.WebUI/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BlogApp.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,9 +10,21 @@ namespace BlogApp.WebUI.Controllers
 {
     public class DashboardController : Controller
     {
+        IBlogService _blogService;
+        ICategoryService _categoryService;
+
+        public DashboardController(IBlogService blogService, ICategoryService categoryService)
+        {
+            _blogService = blogService;
+            _categoryService = categoryService;
+        }
+
         [AllowAnonymous]
         public IActionResult Index()
         {
+            ViewBag.BlogCount = _blogService.GetAll().Data.Count().ToString();
+            ViewBag.AuthorBlogCount = _blogService.GetBlogByAuthor(1).Data.Count().ToString();
+            ViewBag.CategoryCount = _categoryService.GetAll().Data.Count().ToString();
             return View();
         }
     }
