c7f565bf503696d2973b7798ad3fd8f45bbeed3a
berkdemircioglu <berkdemircioglu@yahoo.com>
2022-10-22 02:50:00 +0300

    110.ders bitti

---
+++ BlogApp.WebUI/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,27 @@
+﻿using BlogApp.WebUI.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+            list.Add(new CategoryClass { categoryname = "Software", categorycount = 10  });
+            list.Add(new CategoryClass { categoryname = "Sport", categorycount = 14  });
+            list.Add(new CategoryClass { categoryname = "Technology", categorycount = 5  });
+            return Json(new { jsonlist = list });
+        }
+    }
+}
