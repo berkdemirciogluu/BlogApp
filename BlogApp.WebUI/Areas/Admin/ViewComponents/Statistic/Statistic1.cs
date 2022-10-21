cadc2159fc8d8cf432bd4ac6b59a8eab735d25b0
berkdemircioglu <berkdemircioglu@yahoo.com>
2022-10-21 22:49:27 +0300

    103.ders bitti

--- BlogApp.WebUI/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ BlogApp.WebUI/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace BlogApp.WebUI.Areas.Admin.ViewComponents.Statistic
 {
@@ -25,6 +26,11 @@ namespace BlogApp.WebUI.Areas.Admin.ViewComponents.Statistic
             ViewBag.TotalBlogCount = _blogService.GetAll().Count();
             ViewBag.TotalContactCount = _contactService.GetAll().Count();
             ViewBag.TotalCommentCount = _commentService.GetAll().Count();
+
+            string openWheatherKey = "14ad2aba611dbef9c504b82a127794c5";
+            string connectionToOpenWeather = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + openWheatherKey;
+            XDocument document = XDocument.Load(connectionToOpenWeather);
+            ViewBag.Temperature = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
