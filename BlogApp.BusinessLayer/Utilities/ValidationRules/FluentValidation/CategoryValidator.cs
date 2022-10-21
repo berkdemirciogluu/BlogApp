f2fb8abf3bf5f4e901087f07acb40e809036c135
berkdemircioglu <berkdemircioglu@yahoo.com>
2022-10-21 19:16:02 +0300

    93.ders bitti

---
+++ BlogApp.BusinessLayer/Utilities/ValidationRules/FluentValidation/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using BlogApp.EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.BusinessLayer.Utilities.ValidationRules.FluentValidation
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.Name).MaximumLength(50);
+            RuleFor(x => x.Name).MinimumLength(2);
+        }
+    }
+}
--- BlogApp.WebUI/Areas/Admin/Controllers/CategoriesController.cs
+++ BlogApp.WebUI/Areas/Admin/Controllers/CategoriesController.cs
@@ -1,9 +1,13 @@
 ﻿using BlogApp.BusinessLayer.Abstract;
+using BlogApp.BusinessLayer.Utilities.ValidationRules.FluentValidation;
+using BlogApp.EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace BlogApp.WebUI.Areas.Admin.Controllers
 {
@@ -17,9 +21,34 @@ namespace BlogApp.WebUI.Areas.Admin.Controllers
             _categoryService = categoryService;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int page=1)
         {
-            return View(_categoryService.GetAll());
+            return View(_categoryService.GetAll().ToPagedList(page,3));
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(category);
+            if (results.IsValid)
+            {
+                category.Status = true;
+                _categoryService.Add(category);
+                return RedirectToAction("Index", "Categories");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
